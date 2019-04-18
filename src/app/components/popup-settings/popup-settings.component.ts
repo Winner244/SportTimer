5501dc216cef851c108c6cb535620e4768e40bf9
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2019-04-18 13:34:14 +0700

    styles for popup-settings.component

--- src/app/components/popup-settings/popup-settings.component.ts
+++ src/app/components/popup-settings/popup-settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/internal/operators';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-popup-settings',
@@ -9,7 +10,13 @@ import { takeUntil } from 'rxjs/internal/operators';
   styleUrls: ['./popup-settings.component.less']
 })
 export class PopupSettingsComponent implements OnDestroy {
+  faTimes = faTimes;
+
   isOpen: boolean = false;
+  isDisplayOldResults: boolean = true;
+  isOnPushNotification: boolean = false;
+  isOnGoogleDrive: boolean = false;
+  googleDriveEmail: string;
 
   private _destroyed: Subject<any> = new Subject();
 
@@ -26,5 +33,30 @@ export class PopupSettingsComponent implements OnDestroy {
 
   onClose(){
     this.settingsService.isOpen = false;
+  }
+
+  toggleDisplayOldResults(){
+    this.isDisplayOldResults = !this.isDisplayOldResults;
+  }
+
+  toggleOnPushNotification(){
+    this.isOnPushNotification = !this.isOnPushNotification;
+  }
+  
+
+  removeExercise(exercise: string){
+    console.log('removeExercise: ' + exercise);
+  }
+
+  addExercise(){
+    console.log('addExercise');
+  }
+
+  connectGoogleDrive(){
+    console.log('connectGoogleDrive');
+  }
+  
+  disconnectGoogleDrive(){
+    console.log('disconnectGoogleDrive');
   }
 }
