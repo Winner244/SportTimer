9300333975e93fa707d83567d41b222b5493b9d3
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2019-05-14 08:25:23 +0300

    format app + task

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -3,31 +3,33 @@ import { GoogleDriveService } from 'src/app/services/google-drive.service';
 import * as moment from 'moment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
-  title = 'sportTimer';
-  isShowFooterFixed = false;
+   title = 'sportTimer';
+   isShowFooterFixed = false;
 
-  @ViewChild('content') contentElement: ElementRef;
-  @ViewChild('footer') footerElement: ElementRef;
+   @ViewChild('content') contentElement: ElementRef;
+   @ViewChild('footer') footerElement: ElementRef;
 
-  constructor(private googleDriveService: GoogleDriveService){
-    this.onResize = this.onResize.bind(this);
+   constructor(private googleDriveService: GoogleDriveService) {
+      this.onResize = this.onResize.bind(this);
 
-    googleDriveService.synchronizationDrive();
-  }
+      //синхронизация данных при старте
+      this.googleDriveService.synchronizationDrive();
 
-  ngOnInit() {
-    setTimeout(this.onResize, 100);
+      //локализация времени
+      const language = window.navigator ? window.navigator.language : 'en';
+      moment.locale(language.substr(0, 2));
+   }
 
-    const language = window.navigator ? window.navigator.language : 'en';
-    moment.locale(language.substr(0, 2));
-  }
+   ngOnInit() {
+      setTimeout(this.onResize, 100);
+   }
 
-  onResize() {
-    this.isShowFooterFixed = window.innerHeight > this.contentElement.nativeElement.offsetHeight + this.footerElement.nativeElement.offsetHeight;
-  }
+   onResize() {
+      this.isShowFooterFixed = window.innerHeight > this.contentElement.nativeElement.offsetHeight + this.footerElement.nativeElement.offsetHeight;
+   }
 }
