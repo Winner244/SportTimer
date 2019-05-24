e9e145f3bc9695e83477c672b81fa625ebb63d5f
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2019-05-24 08:03:33 +0300

    fix chart

--- src/app/components/popup-chart/popup-chart.component.ts
+++ src/app/components/popup-chart/popup-chart.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/internal/operators';
 import { Chart } from 'chart.js';
 import { ExerciseResultsService } from 'src/app/services/exercise-results.service';
 import * as moment from 'moment';
+import { ModelExerciseResult } from 'src/app/models/ModelExerciseResult';
 
 @Component({
    selector: 'app-popup-chart',
    templateUrl: './popup-chart.component.html',
    styleUrls: ['./popup-chart.component.less']
 })
-export class PopupChartComponent implements OnDestroy {
+export class PopupChartComponent implements OnDestroy, OnInit {
 
-   isOpen: boolean;
+	isOpen: boolean;
+	chart : Chart;
 
    @ViewChild('exerciseSelectedChart') exerciseSelectedChartElement: ElementRef;
 
@@ -33,13 +35,53 @@ export class PopupChartComponent implements OnDestroy {
    ngOnDestroy() {
       this._destroyed.next();
       this._destroyed.complete();
-   }
+	}
+	
+	ngOnInit(){
+      const ctx = (<any>this.exerciseSelectedChartElement.nativeElement).getContext('2d');
+		this.chart = new Chart(ctx, {
+			type: 'line',
+			data: [],
+			options: {
+				responsive: true,
+				hoverMode: 'index',
+				stacked: false,
+				legend: {
+					position: 'right',
+					labels: {
+						usePointStyle: true,
+						padding: 20,
+						fontSize: 14,
+						boxWidth: 5
+					}
+				},
+				scales: {
+					yAxes: [{
+						type: 'linear', 
+						display: true,
+						position: 'left',
+						id: 'y-axis-1',
+					},
+					{
+						type: 'linear', 
+						display: false,
+						id: 'y-axis-2',
+					}],
+					xAxes: [{
+						type: 'category',
+						labels: [],
+						offset: true
+				  }]
+				}
+			}
+		});
+	}
 
    drawChart(){
-		const selectedExervices = this.exerciseResultsService.getTypeSelectedExerciseResults();
-      const ctx = (<any>this.exerciseSelectedChartElement.nativeElement).getContext('2d');
-		const xAxis= selectedExervices.map(x => moment(x.date).format('DD.MM.YYYY'));
-		const mass = selectedExervices.map(x => x.results.sum(r => r.mass));
+		let selectedExerviceResults: ModelExerciseResult[] = [].concat(this.exerciseResultsService.getTypeSelectedExerciseResults());
+		selectedExerviceResults = selectedExerviceResults.sortByField(x => x.date);
+		const xAxis= selectedExerviceResults.map(x => moment(x.date).format('DD.MM.YYYY'));
+		const mass = selectedExerviceResults.map(x => x.results.sum(r => r.mass));
 
 		const lineChartData = {
 			datasets: [{
@@ -48,7 +90,7 @@ export class PopupChartComponent implements OnDestroy {
 				backgroundColor: 'rgb(1, 0, 187)',
 				pointRadius: 4,
 				fill: false,
-				data: selectedExervices.map(x => x.results.sum(r => r.count)),
+				data: selectedExerviceResults.map(x => x.results.sum(r => r.count)),
 				yAxisID: 'y-axis-1',
 				offset: true
 			}, {
@@ -69,44 +111,9 @@ export class PopupChartComponent implements OnDestroy {
 			lineChartData.datasets = [lineChartData.datasets[0]];
 		}
 
-		Chart.Line(ctx, {
-			data: lineChartData,
-			options: {
-				responsive: true,
-				hoverMode: 'index',
-				stacked: false,
-				legend: {
-					position: 'right',
-					labels: {
-						usePointStyle: true,
-						padding: 20,
-						fontSize: 14,
-						boxWidth: 5
-					}
-				},
-				elements: {
-				},
-				scales: {
-					yAxes: [{
-						type: 'linear', 
-						display: true,
-						position: 'left',
-						id: 'y-axis-1',
-					},
-					{
-						type: 'linear', 
-						display: false,
-						id: 'y-axis-2',
-					}],
-					xAxes: [{
-						type: 'category',
-						labels: xAxis,
-						offset: true
-				  }]
-				}
-			}
-		});
-	
+		this.chart.data = lineChartData;
+		this.chart.options.scales.xAxes[0].labels = xAxis;
+		this.chart.update();
    }
 
    onClose() {
