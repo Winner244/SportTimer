ad012c5371162235f22234204d05b046780f2221
alexander.panfilov <alexander.panfilov@sharp-dev.net>
2019-05-31 09:49:08 +0300

    timer. resolution for < 650 [not final]

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -6,37 +6,42 @@ import { Helper } from '../../Helper';
 import { TimerService } from 'src/app/services/timer.service';
 
 @Component({
-  selector: 'app-timer',
-  templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.less']
+   selector: 'app-timer',
+   templateUrl: './timer.component.html',
+   styleUrls: ['./timer.component.less']
 })
 export class TimerComponent implements OnDestroy {
 
-  private _destroyed: Subject<any> = new Subject();
+   private _destroyed: Subject<any> = new Subject();
 
-  timer: number;
+   timer: number;
+   windowWidth: number;
 
-  constructor(private timerService: TimerService) { 
-    this.timerService.timer$
-      .pipe(takeUntil(this._destroyed))
-      .subscribe(value => this.timer = value);
-  }
+   constructor(private timerService: TimerService) {
+      this.timerService.timer$
+         .pipe(takeUntil(this._destroyed))
+         .subscribe(value => this.timer = value);
+   }
 
-  ngOnDestroy(){
-    this._destroyed.next();
-    this._destroyed.complete();
-  }
+   ngOnDestroy() {
+      this._destroyed.next();
+      this._destroyed.complete();
+   }
 
-  /**
-   * Возвращает строку таймера в формате 00:00:00.00 с запущенного таймера
-   * @return {string}
-   */
-  get timeString(){
-    return '{0}:{1}:{2}.{3}'.format(
-        Helper.twoZero(parseInt(this.timer / 60 / 60 / 1000 + '')),
-        Helper.twoZero(parseInt(this.timer / 60 / 1000 % 60 + '')),
-        Helper.twoZero(parseInt(this.timer / 1000 % 60 + '')),
-        Helper.twoZero(parseInt((this.timer % 1000 / 10) + '')),
-    );
-  }
+   /**
+    * Возвращает строку таймера в формате 00:00:00.00 с запущенного таймера
+    * @return {string}
+    */
+   get timeString() {
+      return '{0}:{1}:{2}.{3}'.format(
+         Helper.twoZero(parseInt(this.timer / 60 / 60 / 1000 + '')),
+         Helper.twoZero(parseInt(this.timer / 60 / 1000 % 60 + '')),
+         Helper.twoZero(parseInt(this.timer / 1000 % 60 + '')),
+         Helper.twoZero(parseInt((this.timer % 1000 / 10) + '')),
+      );
+   }
+
+   public onResize() {
+      this.windowWidth = window.innerWidth;
+   }
 }
